25aff31eeb43ca5eb335602e204e66815bf44413
far1h <farihmuhammad45@gmail.com>
2024-09-19 11:39:42 +0700

    Add User Interface

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currencyNameOne: UILabel!
+    @IBOutlet weak var currencyNameTwo: UILabel!
+    @IBOutlet weak var currencyNumOne: UILabel!
+    @IBOutlet weak var currencyNumTwo: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currencyPicker.dataSource=self
+        currencyPicker.delegate=self
     }
 
 
 }
 
+extension ViewController: UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+    
+}
+
+extension ViewController: UIPickerViewDelegate{
+    
+}
