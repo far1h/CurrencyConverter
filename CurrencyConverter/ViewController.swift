0b3a53b7b1f849bbff7614bb79fe5e933a90c575
far1h <farihmuhammad45@gmail.com>
2024-09-19 12:08:11 +0700

    Create URL

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var currencyNameOne: UILabel!
-    @IBOutlet weak var currencyNameTwo: UILabel!
     @IBOutlet weak var currencyNumOne: UILabel!
-    @IBOutlet weak var currencyNumTwo: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         currencyPicker.dataSource=self
         currencyPicker.delegate=self
+    }
+    
+    func getRates(){
+        // 1) Request & Session
+        // 2) Response & Data
+        // 3) Parsing & JSON Sterialization
+        
+//        1)
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=313adb6cf0ddf4484935be7e12ca1eba")
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url!) { data, response, error in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "OK", style: .default)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+//                2)
+                if data != nil {
+                    
+                }
+            }
+        }
     }
 
 
@@ -25,7 +48,7 @@ class ViewController: UIViewController {
 
 extension ViewController: UIPickerViewDataSource{
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        <#code#>
+        return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
@@ -38,3 +61,5 @@ extension ViewController: UIPickerViewDataSource{
 extension ViewController: UIPickerViewDelegate{
     
 }
+
+
